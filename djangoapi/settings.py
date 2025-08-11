90bbe0c3ab23a76959463a707db0259c90a9991d
WilliamDelnington <107051066+WilliamDelnington@users.noreply.github.com>
2025-08-11 11:05:59 +0700

    Update settings.py: Add allowed hosts with HTTPS

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -31,13 +31,13 @@ SECRET_KEY = 'django-insecure-*d4o(c++2eotgm&myx-0e1f8e&wa#%!vq7$yny6m0$ec=2o3o0
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "simpleauth-e5cnh0cfcxfcdbd0.canadacentral-01.azurewebsites.net",
+    "https://simpleauth-e5cnh0cfcxfcdbd0.canadacentral-01.azurewebsites.net",
     "http://localhost:3000"
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "simpleauth-e5cnh0cfcxfcdbd0.canadacentral-01.azurewebsites.net"
+    "https://simpleauth-e5cnh0cfcxfcdbd0.canadacentral-01.azurewebsites.net"
 ]
 
 
