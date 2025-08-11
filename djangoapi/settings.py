b90327575db2e95f3f129a17afb9cf96d3c9c4f2
Peter Packerson <vunhat20032008@gmail.com>
2025-08-11 11:34:14 +0700

    Fix Allowed Hosts

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -31,11 +31,11 @@ SECRET_KEY = 'django-insecure-*d4o(c++2eotgm&myx-0e1f8e&wa#%!vq7$yny6m0$ec=2o3o0
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "simpleauth-e5cnh0cfcxfcdbd0.scm.canadacentral-01.azurewebsites.net"
+    "simpleauth-e5cnh0cfcxfcdbd0.canadacentral-01.azurewebsites.net"
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://localhost:3000",
     "https://simpleauth-e5cnh0cfcxfcdbd0.scm.canadacentral-01.azurewebsites.net/"
 ]
 
